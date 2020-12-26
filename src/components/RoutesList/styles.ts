674a857fd3523193c43f6f18381f08c19b9be6c4
Elian Batista <elianbatista2000@gmail.com>
2020-12-25 22:39:24 -0300

    alterando o ts do routes list

--- src/components/RoutesList/styles.ts
+++ src/components/RoutesList/styles.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+  width: 25%;
+`
+
 export const InfoBar = styled.div`
-  
+  width: 100%;
+  background-color: var(--primary);
+  font-weight: 600;
+  color: #fff;
+  font-size: 1.5rem;
+  padding: 1.3rem 1.6rem;
 `
