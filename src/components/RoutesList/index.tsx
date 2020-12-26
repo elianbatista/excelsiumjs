81ce24f39d496089dff041c66feac206dd3df0fc
Elian Batista <elianbatista2000@gmail.com>
2020-12-25 22:30:52 -0300

    Começo do routes list

---
+++ src/components/RoutesList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { InfoBar } from './styles'
+
+const RoutesList: React.FC = () => {
+  return (
+    <InfoBar>
+      Excelsiumjs
+    </InfoBar>
+  )
+}
+
+export default RoutesList
