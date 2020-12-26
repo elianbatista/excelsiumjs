2b2903acf20eaa17ce5d99ddeb0aa16d63542527
Elian Batista <elianbatista2000@gmail.com>
2020-12-25 22:13:35 -0300

    add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
-    <h1>Teste</h1>
+    <>
+      <h1>Teste</h1>
+      <GlobalStyles />  
+    </>
   );
 }
 
