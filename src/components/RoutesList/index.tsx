1130926f39ccd90695f0ade778411604b4714519
Elian Batista <elianbatista2000@gmail.com>
2020-12-26 20:09:24 -0300

    index routes list

--- src/components/RoutesList/index.tsx
+++ src/components/RoutesList/index.tsx
@@ -1,16 +1,242 @@
 import React from 'react'
 
-import { Container, InfoBar, FilterBar, FilterField } from './styles'
+import { 
+  Container, 
+  InfoBar, 
+  FilterBar, 
+  FilterField, 
+  WorkspaceList ,
+  PrincipalFolder,
+  Folder,
+  InfoFolder,
+  FolderIcon,
+  FolderTitle,
+  Request,
+  Tipo,
+  Nome
+} from './styles'
 
 const RoutesList: React.FC = () => {
   return (
     <Container>
       <InfoBar>
-        Excelsiumjs
+        excelsiumjs
       </InfoBar>
       <FilterBar>
-        <FilterField />
+        <FilterField placeholder="filter" />
       </FilterBar>
+      <WorkspaceList>
+        <PrincipalFolder>
+          <Folder>
+            <InfoFolder>
+              <FolderIcon />
+              <FolderTitle>
+                Usuários
+              </FolderTitle>
+            </InfoFolder>
+            <Request>
+              <Tipo id="get">
+                get
+              </Tipo>
+              <Nome>
+                List
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="del">
+                del
+              </Tipo>
+              <Nome>
+                Destroy
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="put">
+                put
+              </Tipo>
+              <Nome>
+                Update
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="post">
+                post
+              </Tipo>
+              <Nome>
+                Store
+              </Nome>
+            </Request>
+          </Folder>
+          <Folder>
+            <InfoFolder>
+              <FolderIcon />
+              <FolderTitle>
+                Estabelecimentos
+              </FolderTitle>
+            </InfoFolder>
+            <Request>
+              <Tipo id="get">
+                get
+              </Tipo>
+              <Nome>
+                List
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="del">
+                del
+              </Tipo>
+              <Nome>
+                Destroy
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="put">
+                put
+              </Tipo>
+              <Nome>
+                Update
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="post">
+                post
+              </Tipo>
+              <Nome>
+                Store
+              </Nome>
+            </Request>
+          </Folder>
+          <Folder>
+            <InfoFolder>
+              <FolderIcon />
+              <FolderTitle>
+                Restaurantes
+              </FolderTitle>
+            </InfoFolder>
+            <Request>
+              <Tipo id="get">
+                get
+              </Tipo>
+              <Nome>
+                List
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="del">
+                del
+              </Tipo>
+              <Nome>
+                Destroy
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="put">
+                put
+              </Tipo>
+              <Nome>
+                Update
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="post">
+                post
+              </Tipo>
+              <Nome>
+                Store
+              </Nome>
+            </Request>
+          </Folder>
+          <Folder>
+            <InfoFolder>
+              <FolderIcon />
+              <FolderTitle>
+                Funcionários
+              </FolderTitle>
+            </InfoFolder>
+            <Request>
+              <Tipo id="get">
+                get
+              </Tipo>
+              <Nome>
+                List
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="del">
+                del
+              </Tipo>
+              <Nome>
+                Destroy
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="put">
+                put
+              </Tipo>
+              <Nome>
+                Update
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="post">
+                post
+              </Tipo>
+              <Nome>
+                Store
+              </Nome>
+            </Request>
+          </Folder>
+          <Folder>
+            <InfoFolder>
+              <FolderIcon />
+              <FolderTitle>
+                Empresas
+              </FolderTitle>
+            </InfoFolder>
+            <Request>
+              <Tipo id="get">
+                get
+              </Tipo>
+              <Nome>
+                List
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="del">
+                del
+              </Tipo>
+              <Nome>
+                Destroy
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="put">
+                put
+              </Tipo>
+              <Nome>
+                Update
+              </Nome>
+            </Request>
+            <Request>
+              <Tipo id="post">
+                post
+              </Tipo>
+              <Nome>
+                Store
+              </Nome>
+            </Request>
+          </Folder>
+          <Request>
+            <Tipo id="post">
+              post
+            </Tipo>
+            <Nome>
+              Registrar
+            </Nome>
+          </Request>
+        </PrincipalFolder>
+      </WorkspaceList>
     </Container>
   )
 }
