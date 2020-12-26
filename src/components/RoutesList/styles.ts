09c90e4daa3a2f0bf538ace402c20006b7143f85
Elian Batista <elianbatista2000@gmail.com>
2020-12-26 20:09:38 -0300

    styles routes list

--- src/components/RoutesList/styles.ts
+++ src/components/RoutesList/styles.ts
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
+import { FaRegFolder } from 'react-icons/fa'
 
 export const Container = styled.div`
-  width: 25%;
-  background-color: #44475a;
+  width: 22%;
+  background-color: var(--current);
+  display: flex;
+  flex-direction: column;
 `
 
 export const InfoBar = styled.div`
   width: 100%;
-  background-color: var(--primary);
+  background-color: var(--background);
   font-weight: 600;
-  color: #fff;
+  color: var(--foreground);
   font-size: 1.5rem;
   padding: 1.3rem 1.6rem;
 `
@@ -21,9 +24,94 @@ export const FilterBar = styled.div`
 export const FilterField = styled.input`
   width: 100%;
   background-color: transparent;
-  border: 1px solid #7E83A6;
+  border: 1px solid var(--gray);
   border-radius: 3px;
   padding: 0.6rem 1rem;
   font-size: 1.2rem;
-  color: #7E83A6;
+  color: var(--gray);
+`
+
+export const WorkspaceList = styled.div`
+  height: 100%;
+  overflow-y: scroll;
+  padding: 0rem 1rem;
+
+  ::-webkit-scrollbar {
+    width: 6px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--purple);
+    border-radius: 4px;
+  }
+  ::-webkit-scrolbbar-track {
+    background-color: var(--background);
+  }
+`
+
+export const PrincipalFolder = styled.ul`
+
+`
+
+export const Folder = styled.ul`
+  padding: 0 1.2rem;
+  > li {
+    padding-left: 1.2rem;
+    padding-right: 1.2rem;
+  }
+`
+
+export const Request = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 0.6rem 1.2rem;
+  cursor: pointer;
+  transition: background-color .2s;
+  &:hover {
+    background-color: rgba(255,255,255,0.1);
+  }
+`
+
+export const InfoFolder = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0.6rem 0rem;
+  cursor: pointer;
+`
+
+export const FolderIcon = styled(FaRegFolder)`
+  width: 1.1rem;
+  height: 1.1rem;
+  color: #fff;
+  margin-right: 0.8rem;
+`
+
+export const FolderTitle = styled.h2`
+  font-weight: 500;
+  font-size: 0.95rem;
+  color: var(--light-gray);
+`
+
+export const Tipo = styled.div`
+  text-transform: uppercase;
+  font-size: 0.6rem;
+  width: 2.6rem;
+  color: #fff;
+  &#get {
+    color: var(--purple);
+  }
+  &#post {
+    color: var(--cyan);
+  }
+  &#put {
+    color: var(--pink);
+  }
+  &#del {
+    color: var(--red);
+  }
+`
+
+export const Nome = styled.h3`
+  font-weight: 500;
+  font-size: 0.9rem;
+  color: var(--light-gray);
 `
