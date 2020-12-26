8ce6f5f0680b15c975fce337ef0acddb568f4c77
Elian Batista <elianbatista2000@gmail.com>
2020-12-25 22:30:59 -0300

    alterando o layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import { Container } from './styles'
 
+import RoutesList from '../RoutesList'
+
 const Layout: React.FC = () => {
   return (
     <Container>
-      
+      <RoutesList />
     </Container>
   )
 }
