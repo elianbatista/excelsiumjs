5755aa5de34cbf6981e0fa022eddffb7d2faeb29
Elian Batista <elianbatista2000@gmail.com>
2020-12-25 22:49:19 -0300

    editando o html do routes list

--- src/components/RoutesList/styles.ts
+++ src/components/RoutesList/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 25%;
+  background-color: #44475a;
 `
 
 export const InfoBar = styled.div`
@@ -11,4 +12,18 @@ export const InfoBar = styled.div`
   color: #fff;
   font-size: 1.5rem;
   padding: 1.3rem 1.6rem;
+`
+
+export const FilterBar = styled.div`
+  padding: 1rem;
+`
+
+export const FilterField = styled.input`
+  width: 100%;
+  background-color: transparent;
+  border: 1px solid #7E83A6;
+  border-radius: 3px;
+  padding: 0.6rem 1rem;
+  font-size: 1.2rem;
+  color: #7E83A6;
 `
