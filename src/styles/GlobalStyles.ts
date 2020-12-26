02e52fc2b40ce99c89c8c706367cef901881fec0
Elian Batista <elianbatista2000@gmail.com>
2020-12-26 20:09:44 -0300

    global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500;600;700&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@500;600;700&display=swap');
   
   * {
     margin: 0;
@@ -15,9 +15,22 @@ export default createGlobalStyle`
   *, button, input {
     border: 0;
     outline: 0;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Ubuntu', sans-serif;
   }
   :root {
-    --primary: #6633cc;
+    --background: #282a36;
+    --current: #44475a;
+    --selection: #44475a;
+    --gray: #9E9E9E;
+    --light-gray: #c9d1d9;
+    --foreground: #f8f8f2;
+    --comment: #6272a4;
+    --cyan: #8be9fd;
+    --green: #50fa7b;
+    --orange: #ffb86c;
+    --pink: #ff79c6;
+    --purple: #bd93f9;
+    --red: #ff5555;
+    --yellow: #f1fa8c;
   }
 `
