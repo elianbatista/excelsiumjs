176aec8366a915a599f904cc21415f1d2dd40158
Elian Batista <elianbatista2000@gmail.com>
2020-12-28 22:37:44 -0300

    alteracoes no layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { Container } from './styles'
 
+import NavBar from '../NavBar'
 import RoutesList from '../RoutesList'
 import RequisicaoInfo from '../RequisicaoInfo'
 
 const Layout: React.FC = () => {
   return (
     <Container>
+      <NavBar tipo={2} url="http://localhost:3333/api/pagamento" />
       <RoutesList />
       <RequisicaoInfo />
     </Container>
