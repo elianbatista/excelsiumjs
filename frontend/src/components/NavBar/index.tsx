941c8d54fa907133fe7e54678c4cca853de208b9
Elian Batista <elianbatista2000@gmail.com>
2020-12-29 22:52:23 -0300

    colocando usuarios e github

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
-import { Container, InfoBar } from './styles'
+import { Container, InfoBar, UsuariosList, Usuario, GitHub } from './styles'
 
-const NavBar: React.FC = () => {
+export interface Props {
+  userImagem?: string
+}
+
+const NavBar: React.FC<Props> = ({
+  userImagem
+}) => {
   return (
     <Container>
       <InfoBar>
         excelsiumjs
       </InfoBar>
+      <div className="content">
+        <UsuariosList>
+          <Usuario userImagem={'https://avatars2.githubusercontent.com/u/49127170?s=460&u=c76b3ceb05db028102e9fbb26143471068fc0a47&v=4'} />
+          <Usuario userImagem={'https://avatars2.githubusercontent.com/u/49127170?s=460&u=c76b3ceb05db028102e9fbb26143471068fc0a47&v=4'} />
+          <Usuario userImagem={'https://avatars2.githubusercontent.com/u/49127170?s=460&u=c76b3ceb05db028102e9fbb26143471068fc0a47&v=4'} />
+          <Usuario userImagem={'https://avatars2.githubusercontent.com/u/49127170?s=460&u=c76b3ceb05db028102e9fbb26143471068fc0a47&v=4'} />
+        </UsuariosList>
+        <a href="https://github.com/elianbatista/excelsiumjs" target="_blank">
+          <GitHub />
+        </a>
+      </div>
     </Container>
   )
 }
--- frontend/src/components/NavBar/styles.ts
+++ frontend/src/components/NavBar/styles.ts
@@ -1,13 +1,24 @@
 import styled from 'styled-components'
 
+import { FaGithub } from 'react-icons/fa'
+
+import { Props } from '.'
+
 export const Container = styled.div`
   width: 100%;
   background-color: var(--background);
   box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   height: 4rem;
+  padding: 0 1.6rem;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   z-index: 5;
+
+  > div.content {
+    display: flex;
+    align-items: center;
+  }
 `
 
 export const InfoBar = styled.div`
@@ -15,7 +26,37 @@ export const InfoBar = styled.div`
   font-weight: 600;
   color: var(--foreground);
   font-size: 1.4rem;
-  padding: 0rem 1.6rem;
   z-index: 3;
 `
+
+export const UsuariosList = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 0.2rem;
+  border-right: 1px solid rgba(255,255,255,0.4);
+`
+
+export const Usuario = styled.div<Props>`
+  width: 2.8rem;
+  height: 2.8rem;
+  border-radius: 50%;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${props => props.userImagem});
+  margin-right: 1rem;
+`
+
+export const GitHub = styled(FaGithub)`
+  width: 2rem;
+  height: 2rem;
+  margin-left: 1.2rem;
+  color: rgba(255,255,255,0.4);
+  cursor: pointer;
+  transition: color .2s;
+
+  &:hover {
+    color: rgba(255,255,255,0.6);
+  }
+`
+
 
