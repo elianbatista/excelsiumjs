ace1af7e4230e759d95cf31217840e7b5a26da0b
Elian Batista <elianbatista2000@gmail.com>
2020-12-31 17:22:22 -0300

    create style workspace

--- frontend/src/pages/Workspaces/styles.ts
+++ frontend/src/pages/Workspaces/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { FaEye } from 'react-icons/fa'
 
 export const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   overflow: hidden;
