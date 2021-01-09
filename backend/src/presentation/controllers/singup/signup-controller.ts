5846754263b6cba92cb6c50273e19f20ca0181e1
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-01-09 20:46:33 -0300

    ensure SignUpController returns 400 if no name is provided

--- backend/src/presentation/controllers/singup/signup-controller.ts
+++ backend/src/presentation/controllers/singup/signup-controller.ts
@@ -1,13 +1,21 @@
 import { MissingParamError } from '../../errors/missing-param-error'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest } from '../../helpers/http/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email } = httpRequest.body
+    const { email, password, name } = httpRequest.body
     if (!email) {
       return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    }
+
+    if (!password) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+    }
+
+    if (!name) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('name'))))
     }
   }
 }
--- backend/src/presentation/controllers/singup/singup-controller.spec.ts
+++ backend/src/presentation/controllers/singup/singup-controller.spec.ts
@@ -13,4 +13,28 @@ describe('Sign Up', () => {
     const result = await sut.handle(httpRequest)
     expect(result.statusCode).toBe(400)
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email'
+      }
+    }
+    const result = await sut.handle(httpRequest)
+    expect(result.statusCode).toBe(400)
+  })
+
+  test('Should return 400 if no name is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password',
+        email: 'any_email'
+      }
+    }
+    const result = await sut.handle(httpRequest)
+    expect(result.statusCode).toBe(400)
+  })
 })
