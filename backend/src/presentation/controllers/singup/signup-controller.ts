e669eba1b920172ed4b36658e1a2b7b80ab1a017
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-01-09 20:35:57 -0300

    add badRequest and MissingParamError

--- backend/src/presentation/controllers/singup/signup-controller.ts
+++ backend/src/presentation/controllers/singup/signup-controller.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
@@ -5,10 +7,7 @@ export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { email } = httpRequest.body
     if (!email) {
-      return new Promise(resolve => resolve({
-        statusCode: 400,
-        body: null
-      }))
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
     }
   }
 }
