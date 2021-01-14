f66de155180e99bbdc906c646850ae87702027b9
Elian Batista <elianbatista2000@gmail.com>
2021-01-13 21:26:38 -0300

    alterações no filter bar

--- frontend/src/components/FilterBar/index.tsx
+++ frontend/src/components/FilterBar/index.tsx
@@ -33,9 +33,6 @@ const FilterBar: React.FC = () => {
             <Title>
               Nova Requisição
             </Title>
-            <Title>
-              Nova Requisição
-            </Title>
           </ButtonAdd>
           <ButtonAdd>
             <FolderIcon />
