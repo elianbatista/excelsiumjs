9cccd8cbc26660663a09d09aab713a813cd25b85
Elian Batista <elianbatista2000@gmail.com>
2021-01-13 21:25:31 -0300

    add title

--- frontend/src/components/FilterBar/index.tsx
+++ frontend/src/components/FilterBar/index.tsx
@@ -30,6 +30,9 @@ const FilterBar: React.FC = () => {
             <Title>
               Nova Requisição
             </Title>
+            <Title>
+              Nova Requisição
+            </Title>
           </ButtonAdd>
           <ButtonAdd>
             <FolderIcon />
